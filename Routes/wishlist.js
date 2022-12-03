00b13e9efe14237c2b0e6e99f7ebf71612173d44
6dean <ferdjallahrockdean@gmail.com>
2022-12-04 00:04:47 +0100

    condition token

--- Routes/wishlist.js
+++ Routes/wishlist.js
@@ -6,11 +6,15 @@ const Member = require("../Models/Member");
 router.post("/wishlist", async (req, res) => {
   const { token } = req.body;
   try {
-    const User = await Member.findOne({
-      token: token,
-    });
-    const wishlistArray = User.wishlist;
-    res.status(200).json(wishlistArray);
+    if (token) {
+      const User = await Member.findOne({
+        token: token,
+      });
+      const wishlistArray = User.wishlist;
+      res.status(200).json(wishlistArray);
+    } else {
+      null;
+    }
   } catch (error) {
     res.status(406).json({ message: error });
   }
